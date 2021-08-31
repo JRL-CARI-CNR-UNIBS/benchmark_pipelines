f85c9cb65030ddce299eb0f9cca8ff53ce406ef2
Manuel Beschi <manuel.beschi@unibs.it>
2021-08-31 15:16:55 +0200

    violin plots

--- scripts/view_results.py
+++ scripts/view_results.py
@@ -10,14 +10,17 @@ import rospy
 import numpy  as np
 import math
 import matplotlib.pyplot as plt
-
+import seaborn
+import pandas as pd
 
 load_from_parameter_server=False
+dof=6
+test_name='benchmark_result_'+str(dof)+'.yaml'
 
 if load_from_parameter_server:
    param=rospy.get_param("/benchmark")
 else:
-   with open('benchamrk6dof.yaml') as f:
+   with open(test_name) as f:
        param = yaml.safe_load(f)
 
 queries_number=param["queries_number"]
@@ -26,7 +29,6 @@ query_prefix=param["query_test"]
 pipeline_ids=param["pipeline_ids"]
 
 planning_times=param["planning_times"]
-# planning_times=planning_times[0:3]
 tested_planners=[]
 planner_str=[]
 for pipeline_id in pipeline_ids:
@@ -36,44 +38,29 @@ for pipeline_id in pipeline_ids:
         planner_str.append(planner_id)
 
 tested_planners=tested_planners[0:2]
-tot_min_length=0
-tot_median_length=np.empty([len(tested_planners),len(planning_times)])
-tot_median_length[:]=0
+cost_over_time=[]
+planner_failures=[]
 
-#cost_over_time=np.empty([len(planning_times),len(tested_planners),queries_number])
-median_cost_over_time=[]
 
-tot_failures=np.zeros([len(tested_planners),len(planning_times)])
-tot_planning_time=np.zeros([len(tested_planners),len(planning_times)])
-marker=[]
-
-for iquery in range(1,queries_number):
+planner_name=["Informed RRT*","Mixed-strategy RRT*"]
+for iquery in range(0,queries_number):
     query_str="query_"+str(iquery)
     q=param[query_prefix][query_str];
 
 
     min_length=math.inf
-    median_length=np.empty([len(tested_planners),len(planning_times)])
     lengths=np.empty([len(tested_planners),len(planning_times),repetitions])
-    median_length[:]=np.Inf
     failures=np.zeros([len(tested_planners),len(planning_times)])
-    query_planning_time=np.zeros([len(tested_planners),len(planning_times)])
 
     for iplanner in range(0,len(tested_planners)):
         pipeline=tested_planners[iplanner][0]
         planner=tested_planners[iplanner][1]
-        if (pipeline=="dirrt"):
-            marker.append("d")
-        else:
-            marker.append("o")
         for iplan_time in range(0,len(planning_times)):
             planning_time=planning_times[iplan_time]
             planning_time_string='planning_time_ms_'+str(int(1000*planning_time))
-
             trajectory_length=np.zeros(repetitions)
 
             for iteration in range(0,repetitions):
-
                 iteration_str="iteration_"+str(iteration)
                 try:
                     result=q[pipeline][planner][iteration_str][planning_time_string]
@@ -83,81 +70,23 @@ for iquery in range(1,queries_number):
                     else:
                         trajectory_length[iteration]=np.inf
                         failures[iplanner][iplan_time]+=1
-                    tot_planning_time[iplanner][iplan_time]+=result["planning_time"]
-                    query_planning_time[iplanner][iplan_time]+=result["planning_time"]
                 except:
                     trajectory_length[iteration]=np.inf
-                    tot_planning_time[iplanner][iplan_time]=np.inf
-                    query_planning_time[iplanner][iplan_time]=np.inf
             min_length=min(min_length,np.min(trajectory_length))
-            median_length[iplanner][iplan_time]=np.median(trajectory_length)
             lengths[iplanner][iplan_time]=trajectory_length
-            tot_median_length[iplanner][iplan_time]+=median_length[iplanner][iplan_time]
-            tot_failures[iplanner][iplan_time]+=failures[iplanner][iplan_time]
-    fig, axs = plt.subplots(2)
-    fig.set_size_inches(10,10)
-    for iplanner in range(0,len(tested_planners)):
-        #axs[0].semilogy(query_planning_time[iplanner]/repetitions,np.log10(median_length[iplanner]/min_length),"-"+marker[iplanner], label=planner_str[iplanner])
-        axs[0].semilogy(query_planning_time[iplanner]/repetitions,(median_length[iplanner]/min_length),"-"+marker[iplanner], label=planner_str[iplanner])
-        axs[1].plot(np.array(planning_times),failures[iplanner],marker[iplanner], label=planner_str[iplanner])
 
     for iplanner in range(0,len(tested_planners)):
         for iplan_time in range(0,len(planning_times)):
             for iteration in range(0,repetitions):
-                entry={'planning_time': planning_times[iplan_time],
-                   'planner': tested_planners[iplanner][1],
-                   'length': min(median_length[iplanner][iplan_time]/min_length,10),
+                entry={'Planning time [s]': planning_times[iplan_time],
+                   'planner': planner_name[iplanner],
+                   'Normalized length': min(lengths[iplanner][iplan_time][iteration]/min_length,10),
                    }
                 cost_over_time.append(entry)
-    for iplanner in range(0,len(tested_planners)):
-        for iplan_time in range(0,len(planning_times)):
-            
-            #cost_over_time[iplan_time][iplanner][iquery]=median_length[iplanner][iplan_time]/min_length
-            entry={'planning_time': planning_times[iplan_time],
-                   'planner': tested_planners[iplanner][1],
-                   'length': min(median_length[iplanner][iplan_time]/min_length,10),
-                   }
-            median_cost_over_time.append(entry)
-    axs[0].legend()
-    axs[0].set(xlabel="Max planning time",ylabel="Path length/min(path length))",title=str(query_prefix+"_"+query_str))
-    axs[0].grid(True)
-    axs[1].set(xlabel="Max planning time",ylabel="Failures")
-    axs[1].legend()
-    axs[1].grid(True)
-    fig.savefig(query_prefix+query_str+".png",dpi=300)
-    # plt.show(block=False)
-    # plt.pause(1)
-    # plt.close()
 
-    tot_min_length+=min_length
-
-
-fig, axs = plt.subplots(2)
-fig.set_size_inches(10,10)
-for iplanner in range(0,len(tested_planners)):
-   axs[0].semilogy(tot_planning_time[iplanner]/queries_number/repetitions,(tot_median_length[iplanner]/tot_min_length),"-"+marker[iplanner], label=planner_str[iplanner])
-   #axs[0].semilogy(tot_planning_time[iplanner]/queries_number/repetitions,np.log10(tot_median_length[iplanner]/tot_min_length),"-"+marker[iplanner], label=planner_str[iplanner])
-   axs[1].plot(np.array(planning_times),tot_failures[iplanner],marker[iplanner], label=planner_str[iplanner])
-axs[0].legend()
-axs[0].set(xlabel="Max planning time",ylabel="Path length/min(path length)",title=str(query_prefix+"_sum"))
-axs[0].grid(True)
-axs[1].set(xlabel="Max planning time",ylabel="Failures")
-axs[1].legend()
-axs[1].grid(True)
-
-fig.savefig(query_prefix+"_sum"+".png",dpi=300)
-# plt.show(block=False)
-# plt.pause(1)
-# plt.close()
-
-
-# fig, axs = plt.subplots(1)
-# fig.set_size_inches(10,10)
-# for iplanner in range(0,len(tested_planners)):
-#    axs.plot(np.array(planning_times),tot_planning_time[iplanner]/queries_number/repetitions,"-"+marker[iplanner], label=planner_str[iplanner])
-# axs.legend()
-# axs.set(xlabel="Max planning time",ylabel="Planning time",title=str(query_prefix+"_sum"))
-# axs.grid(True)
-#
-# fig.savefig(query_prefix+"_planning_time"+".png",dpi=300)
-# plt.show()
+a4_dims = (11.7, 8.27)
+db1=pd.DataFrame(cost_over_time)
+fig, ax = plt.subplots(figsize=a4_dims);
+seaborn.violinplot(x="Planning time [s]",y="Normalized length",hue="planner",data=db1,split=True, inner=None,cut=False,scale="count")
+plt.grid()
+fig.savefig("violin_dof"+str(dof)+".png",dpi=300)
